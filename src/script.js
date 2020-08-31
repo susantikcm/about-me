b755062b3e54d8c4982f6b6cc910202df2dfcc59
susantikcm <susantikcm@yahoo.com>
2020-08-31 11:13:09 +1000

    Combine all projects as one website with navigation

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function goBack() {
+    window.history.back();
+}
+
+function enableInstrumentInput () {
+    var isInstrumentChecked = document.getElementById("instrument-check").checked;
+    document.getElementById("instrument-input").disabled = !isInstrumentChecked;
+    e
+}
